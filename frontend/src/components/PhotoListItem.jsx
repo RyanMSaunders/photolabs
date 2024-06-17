77dbc77e32fdaabc24533bb94c2a47a24bab864b
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-06-17 12:14:54 -0400

    Map PhotoListItem to PhotoList

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -12,8 +12,9 @@ const PhotoListItem = (props) => {
   return (
     <article className="photo-list__item">
       <img src={props.photo.imageSource} alt="Photo" className="photo-list__image"/>
-      <img src={props.photo.profile} alt="Profile photo" className="photo-list__user-profile"/>
+      
       <div className="photo-list__user-details">
+        <img src={props.photo.profile} alt="Profile photo" className="photo-list__user-profile"/>
         <p className="photo-list__user-info">{props.photo.username}</p>
         <p className="photo-list__user-location">{props.photo.location.city}, {props.photo.location.country}</p>
       </div>
