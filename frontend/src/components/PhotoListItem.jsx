fcb391616d1861a66137b3998a6314a3e5503e31
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-06-17 12:28:01 -0400

    Refactor prop names in PhtoListItem.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,14 +8,15 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   /* Insert React */
   // props = sampleDataForPhotoListItem;
+  
 
   return (
     <article className="photo-list__item">
-      <img src={props.photo.imageSource} alt="Photo" className="photo-list__image"/>
+      <img src={props.photo.urls.regular} alt="Photo" className="photo-list__image"/>
       
       <div className="photo-list__user-details">
-        <img src={props.photo.profile} alt="Profile photo" className="photo-list__user-profile"/>
-        <p className="photo-list__user-info">{props.photo.username}</p>
+        <img src={props.photo.user.profile} alt="Profile photo" className="photo-list__user-profile"/>
+        <p className="photo-list__user-info">{props.photo.user.name}</p>
         <p className="photo-list__user-location">{props.photo.location.city}, {props.photo.location.country}</p>
       </div>
 
