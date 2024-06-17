e72b06f524c163825824f52e9d6edf472f43bc4c
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-06-17 15:38:29 -0400

    Refactor handClick for FavIcon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,16 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
