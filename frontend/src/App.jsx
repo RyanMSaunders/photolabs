c71b2b0d5621a0da8e1f3f5b450d826c751fc4c8
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-06-18 11:37:13 -0400

    Add styling to Nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopicListItem from 'components/TopicListItem';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      < TopicList />
+      <TopNavigation />
       <PhotoList/>
     </div>
   );
